8023ae40a407a39252ee3ec3de618f9a557aa83f
abhijeet <abhikumarrai@hotmail.com>
2024-08-03 00:22:58 +0530

    removed unwanted code

--- basics1/App.js
+++ basics1/App.js
@@ -1,16 +1,3 @@
-// const heading = React.createElement("h1", { id: "title", className: "class" }, "Namaste Everyone!");
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-
-// root.render(heading);
-
-// const parent = React.createElement(
-//   "div", { id: "parent" },
-//   React.createElement("div", { id: "child" },
-//     React.createElement("h1", {}, "I'am an H1 tag")
-//   )
-// );
-
 const parent = React.createElement("div", { id: "parent" },
   [
     React.createElement("div", { className: "child" },
